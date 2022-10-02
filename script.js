038181bcaff86b69946f4f23bf878146546b2ddd
Fuad303 <sadiqovfuad8@gmail.com>
2022-10-02 19:32:35 +0400

    some possibly meaningful message

---
+++ script.js
@@ -0,0 +1,10 @@
+let navList = document.getElementById('responsiveList');
+
+function myFunction(){
+    if(navList.style.display === "block"){
+        navList.style.display = "none";
+    }
+    else{
+        navList.style.display = "block"
+    }
+}
